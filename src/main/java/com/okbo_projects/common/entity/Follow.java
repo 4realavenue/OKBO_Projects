374e98723512a418f4172de35faa66bb7ff71781
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-21 20:42:41 +0900

    add: Follow 엔티티 내용 추가
    User 구현 이후 주석 처리 된 내용으로 변경 예정
    - follower : 팔로우를 거는 사람(주체)
    - following : 팔로우 할 사람(대상)

--- src/main/java/com/okbo_projects/common/entity/Follow.java
+++ src/main/java/com/okbo_projects/common/entity/Follow.java
@@ -1,4 +1,35 @@
 package com.okbo_projects.common.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "follows")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "follower_id", nullable = false)
+//    private User follower;
+    private Long follower;
+
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "following_id", nullable = false)
+//    private User following;
+    private Long following;
+
+//    public Follow(User follower, User following) {
+//        this.follower = follower;
+//        this.following = following;
+//    }
+    public Follow(Long follower, Long following) {
+        this.follower = follower;
+        this.following = following;
+    }
 }
