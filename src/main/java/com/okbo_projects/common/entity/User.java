44dc23de5b0cf0522d9928218eb70df09b63849e
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-22 21:00:46 +0900

    feat-user: user 엔티티 생성

--- src/main/java/com/okbo_projects/common/entity/User.java
+++ src/main/java/com/okbo_projects/common/entity/User.java
@@ -1,4 +1,31 @@
 package com.okbo_projects.common.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false, unique = true)
+    String nickname;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    public User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
 }
