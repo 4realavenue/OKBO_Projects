ae3585735603a77a3b1701f9883545a94652d6ac
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-26 10:47:52 +0900

    feat: 게시글 조회 response에 좋아요 수, 생성일, 수정일 필드 추가
    ※ likes 구현 후, 게시글별 좋아요 수 가져오도록 수정 (현재 모든 게시글에 좋아요 5로 고정함)

--- src/main/java/com/okbo_projects/domain/board/model/response/BoardReadAllPageResponse.java
+++ src/main/java/com/okbo_projects/domain/board/model/response/BoardReadAllPageResponse.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,13 +15,19 @@ public class BoardReadAllPageResponse {
     private String title;
     private String team;
     private String writer;
+     private Long likes;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public static BoardReadAllPageResponse from(BoardDto boardDto) {
         return new BoardReadAllPageResponse(
                 boardDto.getId(),
                 boardDto.getTitle(),
                 boardDto.getTeam().getTeamName(),
-                boardDto.getWriter()
+                boardDto.getWriter(),
+                5L, //TODO : likes 구현 후, 게시글별 좋아요 수 가져오도록 수정
+                boardDto.getCreatedAt(),
+                boardDto.getModifiedAt()
         );
     }
 }
