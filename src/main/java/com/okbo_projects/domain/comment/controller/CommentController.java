be976d4709af2455bbb88899b7027016388f1d24
dandylsj <lsjyahoo@gmail.com>
2025-11-27 11:45:50 +0900

    refactor:불필요한 import문 삭제

--- src/main/java/com/okbo_projects/domain/comment/controller/CommentController.java
+++ src/main/java/com/okbo_projects/domain/comment/controller/CommentController.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
